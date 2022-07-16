44a851cd156e5ef5457c28e4f9aa904dbb81eb37
Slaventy1969 <slaventy.69@gmail.com>
2022-07-16 18:06:28 +0300

    Implemented the Button component

--- todo_new/src/components/SignUpForm/SignUpForm.js
+++ todo_new/src/components/SignUpForm/SignUpForm.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react'
 import './SignUpForm.css'
 import Input from './Input';
+import Button from './Button';
 
 class SignUpForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             userName: '',
+            users: localStorage.getItem('Users') || [],
         }
         this.onNameChangeHandler = this.onNameChangeHandler.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.addUserOnClickHandler = this.addUserOnClickHandler.bind(this);
+
     }
 
     onNameChangeHandler(e) {
         this.setState({ userName: e.target.value })
     }
+    addUserOnClickHandler() {
+        if (!this.state.users.includes(this.state.userName)) {
+            this.state.users.push(this.state.userName)
+            localStorage.setItem('Users', JSON.stringify(this.state.users))
+        }
+
+    }
     handleKeyPress(e) {
         if (e.key === 'Enter') {
-            console.log('enter');
+            this.addUserOnClickHandler()
         }
     }
+
 
     render() {
         const userName = this.state.userName;
         const maxLength = 6;
         const error = 'Довжина має бути не більше: ' + maxLength + ' [' + maxLength + '/' + userName.length + ']';
+        let isBtnDisabled = '';
+        (userName.length !== 0) ? isBtnDisabled = '' : isBtnDisabled = 'disabled'
+
+
 
         return (
             <>
@@ -38,7 +54,12 @@ class SignUpForm extends Component {
                         onChange={this.onNameChangeHandler}
                         onKeyPress={this.handleKeyPress}
                     />
-
+                    <Button
+                        children="Увійти"
+                        className="enter"
+                        onClick={this.addUserOnClickHandler}
+                        disabled={isBtnDisabled}
+                    />
                 </div>
                 <p>State of Component</p>
                 <pre>{JSON.stringify(this.state, null, 2)}</pre>
@@ -47,5 +68,5 @@ class SignUpForm extends Component {
         )
     }
 }
-//Implemented the Input component
+//Implemented the Button component
 export default SignUpForm
